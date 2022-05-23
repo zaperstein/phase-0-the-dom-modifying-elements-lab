775c3422e37a6360f37bc2ad85e256e3ab527e41
Maggie Saperstein <maggie.saperstein@gmail.com>
2022-05-23 14:26:48 -0500

    commit tree a313ca00abc9444e7240e41bb9f9f0a02e1641b2

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+// // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = "Maggie is the champion";
+document.body.append(newHeader);
+
+
